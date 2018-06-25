6b718bd5db7298ac62452358531e07e858e68fc8
Kirk Johnson <kirkj@hawaii.edu>
2018-06-25 07:41:57 -1000

    initial commit

---
+++ answer-displayer.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Answer Displayer
+Plugin URI: 
+GitHub Plugin URI: 
+Description: Allows you to display answers from multiple peopel to questions you have created.  
+             Very useful if displaying a comparison of answers to a questionnaire.
+Version: 0.0.1
+*/
